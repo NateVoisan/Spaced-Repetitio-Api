28e2a87a2a1c2315bd316816060cb626c03f3e20
Nate Voisan <rufio96@gmail.com>
2020-12-14 17:09:30 -0800

    first commit

--- src/user/user-service.js
+++ src/user/user-service.js
@@ -46,7 +46,7 @@ const UserService = {
       const [languageId] = await trx
         .into('language')
         .insert([
-          { name: 'French', user_id },
+          { name: 'Spanish', user_id },
         ], ['id'])
 
       // when inserting words,
@@ -58,14 +58,14 @@ const UserService = {
         .first()
 
       const languageWords = [
-        ['entraine toi', 'practice', 2],
-        ['bonjour', 'hello', 3],
-        ['maison', 'house', 4],
-        ['développeur', 'developer', 5],
-        ['traduire', 'translate', 6],
-        ['incroyable', 'amazing', 7],
-        ['chien', 'dog', 8],
-        ['chat', 'cat', null],
+        ['azul', 'blue', 2],
+        ['arroz', 'rice', 3],
+        ['magnifico', 'magnificent', 4],
+        ['pelota', 'ball', 5],
+        ['leche', 'milk', 6],
+        ['agua', 'water', 7],
+        ['limon', 'lemon', 8],
+        ['oso', 'bear', null],
       ]
 
       const [languageHeadId] = await trx
--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -172,17 +172,17 @@ describe('User Endpoints', function () {
           name: 'test name',
         }
         const expectedList = {
-          name: 'French',
+          name: 'Spanish',
           total_score: 0,
           words: [
-            { original: 'entraine toi', translation: 'practice' },
-            { original: 'bonjour', translation: 'hello' },
-            { original: 'maison', translation: 'house' },
-            { original: 'développeur', translation: 'developer' },
-            { original: 'traduire', translation: 'translate' },
-            { original: 'incroyable', translation: 'amazing' },
-            { original: 'chien', translation: 'dog' },
-            { original: 'chat', translation: 'cat' },
+            { original: 'azul', translation: 'blue' },
+            { original: 'arroz', translation: 'rice' },
+            { original: 'magnificent', translation: 'magnifico' },
+            { original: 'pelota', translation: 'ball' },
+            { original: 'leche', translation: 'milk' },
+            { original: 'agua', translation: 'water' },
+            { original: 'lemon', translation: 'limon' },
+            { original: 'bear', translation: 'oso' },
           ]
         }
         return supertest(app)
